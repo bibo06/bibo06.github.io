93227c9aad6bbb9267f5435e14d1e06a0f4cb710
biboda <huy.nd1456@arena-multimedia.edu.vn>
2022-04-25 12:53:58 +0700

    up

---
+++ js/bootstrap.js
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>btvn3</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.js"></script>
+	
+</body>
+</html>
